6808f2b9f4218f7d46f9c5045fa968ac53bacf91
uni <cbcb9995@naver.com>
2026-07-15 22:15:03 +0900

    chore: Spring Boot Setting

---
+++ src/main/java/com/bwm/BwmApplication.java
@@ -0,0 +1,13 @@
+package com.bwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwm/BwmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
