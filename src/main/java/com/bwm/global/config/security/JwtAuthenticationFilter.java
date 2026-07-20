35a9b91b8268d99a4d09e8b10009ff081bdde649
uni <cbcb9995@naver.com>
2026-07-20 17:00:06 +0900

    feat: apply role-based auth with jwt

--- src/main/java/com/bwm/global/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/bwm/global/config/security/JwtAuthenticationFilter.java
@@ -7,11 +7,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -21,29 +23,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        
-        // 1. Request Header에서 토큰 추출
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
         String jwt = resolveToken(request);
 
-        // 2. validateToken으로 토큰 유효성 검사
-        if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
-            // 유효한 토큰이면 JwtProvider를 통해 Authentication 객체를 가져와서 SecurityContext에 저장
+        if (StringUtils.hasText(jwt)
+                && jwtProvider.validateToken(jwt)
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
+
             Authentication authentication = jwtProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
-        // 3. 다음 필터로 이동
         filterChain.doFilter(request, response);
     }
 
-    // Request Header에서 토큰 정보 추출
     private String resolveToken(HttpServletRequest request) {
+
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+
+        if (StringUtils.hasText(bearerToken)
+                && bearerToken.startsWith(BEARER_PREFIX)) {
+
             return bearerToken.substring(BEARER_PREFIX.length());
         }
+
         return null;
     }
 }
