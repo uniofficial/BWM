3fc8c0f25cb0625ba55344867710b834b42f3c43
choi <jungchoi1@naver.com>
2026-07-20 16:22:02 +0900

    JWT access Token생성 및 검증

--- src/main/java/com/bwm/auth/service/AuthService.java
+++ src/main/java/com/bwm/auth/service/AuthService.java
@@ -21,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class AuthService {
 
