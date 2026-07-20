09de9e144eac2ae44575dc9585a6e2a5a6f90596
choi <jungchoi1@naver.com>
2026-07-20 14:36:00 +0900

    feat: spring security추가 및 임시토큰 반환

--- src/main/java/com/bwm/auth/dto/LoginRequest.java
+++ src/main/java/com/bwm/auth/dto/LoginRequest.java
@@ -2,11 +2,13 @@ package com.bwm.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     @NotBlank
     @Email
